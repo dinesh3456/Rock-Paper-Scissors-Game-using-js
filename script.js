0332ec9b1ea0340df4df4f5f05a4c6619d27c5ee
Dinesh Ravishankar <45628943+dinesh3456@users.noreply.github.com>
2023-08-06 16:18:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-const rockButton = document.getElementById('rock');
-const scissorsButton = document.getElementById('scissors');
-const paperButton = document.getElementById('paper');
-const roundResult = document.getElementById('round-result');
+const buttons = document.querySelectorAll("button");
+const roundResult = document.getElementById("round-result");
 
-let playerScore =0;
+let playerScore = 0;
 let computerScore = 0;
-const string1 = "rock";
-const string2 = "paper";
-const string3 = "scissor";
 
-let NumberOfRound = prompt("How many rounds do you want to play: 3 or 5?");
-switch(NumberOfRound){
-    case 3:
-        for(i=0;i<3;i++){
-            let choice = prompt("Enter your choice -rock, paper, scissor?");
-            let computerChoice ;
-            const randomIndex = Math.floor(Math.random()*3);
-            if(randomIndex ===0 ){
-                computerChoice = string1;
-            }
-            else if(randomIndex ===1){
-                computerChoice === string2;
-            }
-            else if(randomIndex === 2){
-                computerChoice === string3;
-            }
-
-            if(computerChoice===choice){
-                prompt("No point");
-            }
-            else if(computerChoice==="rock")
-        }
+function computerPlay() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
 }
+
+function playRound(playerChoice, computerChoice) {
+  const roundResultElement = document.getElementById("round-result");
+
+  if (playerChoice === computerChoice) {
+    roundResultElement.textContent = "It's a tie!";
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    roundResultElement.textContent = `You win! ${playerChoice} beats ${computerChoice}.`;
+    playerScore++;
+  } else {
+    roundResultElement.textContent = `You lose! ${computerChoice} beats ${playerChoice}.`;
+    computerScore++;
+  }
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const playerChoice = button.id;
+    const computerChoice = computerPlay();
+    playRound(playerChoice, computerChoice);
+    setTimeout(() => {
+      roundResult.textContent = "Choose your weapon";
+    }, 2000);
+  });
+});
